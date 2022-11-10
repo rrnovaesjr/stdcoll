7477d282572e6daa916376dbd4f62ceb9eb8901b
Rodrigo NOvaes <rodrigo.novaes.jr@gmail.com>
2022-11-10 10:54:31 +0100

    chore: Code format

--- test/stdcoll/linked_list_test.c
+++ test/stdcoll/linked_list_test.c
@@ -8,49 +8,53 @@ stdllist *llist;
 
 const char description[] = "linked_list_test";
 
-void initialize_list() {
+void initialize_list()
+{
     llist = LinkedList();
 }
 
-void destroy_list() {
+void destroy_list()
+{
     LinkedListDelete(llist);
 }
 
-int * supply_int(int n) {
+int *supply_int(int n)
+{
     int *pn = malloc(sizeof(int));
     *pn = n;
     return pn;
 }
 
-void should_initialize_linked_list() {
-    ASSERTI_EQ(LinkedListSize(llist), (size_t) 0);
+void should_initialize_linked_list()
+{
+    ASSERTI_EQ(LinkedListSize(llist), (size_t)0);
     ASSERT_TRUE(LinkedListIsEmpty(llist));
 }
 
-void should_update_linked_list_and_query_elements() {
+void should_update_linked_list_and_query_elements()
+{
     ASSERT_TRUE(LinkedListIsEmpty(llist));
     ASSERT_TRUE(LinkedListAdd(llist, supply_int(1)));
     ASSERT_TRUE(LinkedListAdd(llist, supply_int(2)));
     ASSERT_TRUE(LinkedListAdd(llist, supply_int(3)));
     ASSERT_TRUE(LinkedListAdd(llist, supply_int(4)));
-    ASSERTI_EQ((int) LinkedListSize(llist), 4);
+    ASSERTI_EQ((int)LinkedListSize(llist), 4);
     ASSERT_FALSE(LinkedListIsEmpty(llist));
-    ASSERTI_EQ(*(int *) LinkedListGetAtIndex(llist, 0), 1);
-    ASSERTI_EQ(*(int *) LinkedListGetAtIndex(llist, 1), 2);
-    ASSERTI_EQ(*(int *) LinkedListGetAtIndex(llist, 2), 3);
-    ASSERTI_EQ(*(int *) LinkedListGetAtIndex(llist, 3), 4);
+    ASSERTI_EQ(*(int *)LinkedListGetAtIndex(llist, 0), 1);
+    ASSERTI_EQ(*(int *)LinkedListGetAtIndex(llist, 1), 2);
+    ASSERTI_EQ(*(int *)LinkedListGetAtIndex(llist, 2), 3);
+    ASSERTI_EQ(*(int *)LinkedListGetAtIndex(llist, 3), 4);
     ASSERT_SAME(LinkedListGetAtIndex(llist, -1), NULL);
     ASSERT_SAME(LinkedListGetAtIndex(llist, 100), NULL);
     LinkedListClear(llist);
     ASSERT_TRUE(LinkedListIsEmpty(llist));
-    ASSERTI_EQ((int) LinkedListSize(llist), 0);
+    ASSERTI_EQ((int)LinkedListSize(llist), 0);
 }
 
 test_instance tests[] = {
-    { should_initialize_linked_list, "should_initialize_linked_list" },
-    { should_update_linked_list_and_query_elements, "should_update_linked_list_and_query_elements" },
-    { NULL, NULL }
-};
+    {should_initialize_linked_list, "should_initialize_linked_list"},
+    {should_update_linked_list_and_query_elements, "should_update_linked_list_and_query_elements"},
+    {NULL, NULL}};
 
 void (*before_all)(void) = NULL;
 
